9f63049b9842cfadc06ee30e42986cc7f8cbe576
ommakwanaa <om.makwana@einfochips.com>
2022-04-14 21:23:16 +0530

    updating

--- network_assignment_1/1_client.c
+++ network_assignment_1/1_client.c
@@ -6,35 +6,43 @@
 #include <stdlib.h>
 
 #define PORT 8000
-#define MAX_SIZE 100
-
+#define MAXLINE 1024
 int main()
 {
-    char msg1[MAX_SIZE];
-    char msg2[MAX_SIZE];
-    int sockfd, ret_val;
-    int n;
-    socklen_t addr_len;
+    int sockfd;
+    char buffer[MAXLINE];
+    char *hello = "Hello from client";
+    char *buf;
     struct sockaddr_in servaddr;
-    sockfd = socket(AF_INET, SOCK_DGRAM, 0);
 
-    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    // Creating socket file descriptor
+    if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0)
+    {
+        perror("socket creation failed");
+        exit(EXIT_FAILURE);
+    }
+
     memset(&servaddr, 0, sizeof(servaddr));
+
+    // Filling server information
     servaddr.sin_family = AF_INET;
-    servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
     servaddr.sin_port = htons(PORT);
-    connect(sockfd, (struct sockaddr *)&servaddr, sizeof(servaddr));
+    servaddr.sin_addr.s_addr = INADDR_ANY;
+
     while (1)
     {
-        printf("Enter message for server: \n");
-        fgets(msg1, MAX_SIZE, stdin);
-        if (msg1[0] == '#')
-            break;
-        n = strlen(msg1) + 1;
-        send(sockfd, msg1, n, 0);
+        int n, len;
+        printf("enter msg:");
+        gets(buffer);
+        sendto(sockfd, (const char *)buffer, strlen(buffer), MSG_CONFIRM, (const struct sockaddr *)&servaddr, sizeof(servaddr));
 
-        n = recv(sockfd, msg2, MAX_SIZE, 0);
-        printf("recived msg form server:%s\n", msg2);
+        // sendto(sockfd, (const char *)hello, strlen(hello), MSG_CONFIRM, (const struct sockaddr *) &servaddr, sizeof(servaddr));
+        printf("Hello message sent.\n");
+
+        n = recvfrom(sockfd, (char *)buffer, MAXLINE, MSG_WAITALL, (struct sockaddr *)&servaddr, &len);
+        buffer[n] = '\0';
+        printf("Server : %s\n", buffer);
     }
+    // close(sockfd);
     return 0;
 }
